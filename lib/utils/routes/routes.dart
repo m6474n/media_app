357bc8079db48e2f675a8475aee644681f646cca
Muhammad Mohsin <m.mohsin2055@gmail.com>
2023-01-14 20:31:30 +0500

    initial commit

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:flutter/material.dart';
+import 'package:tech_media/utils/routes/route_name.dart';
+import 'package:tech_media/view/splash/splash_screen.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final arguments = settings.arguments;
+    switch (settings.name) {
+      case RouteName.splashScreen:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          );
+        });
+    }
+  }
+}
